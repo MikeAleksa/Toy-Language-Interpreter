e06a99fd8d30371b1c609c2f642499de07ab339a
mike <mtaleksa1@gmail.com>
2019-03-15 23:41:24 -0400

    rewrite tokenizer to return single tokens at a time as a tuple  (token, type)
    remove whitespace in constructor of tokenizer
    
    begin writing documentation for interpreter

--- Tokenizer.py
+++ Tokenizer.py
@@ -3,57 +3,50 @@
 
 import re
 
-# dict contains the name of the token type (key) and a compiled regex (value) for all tokens in the Toy Language
-# Digit, NonZeroDigit, and Letter are not defined here since they are only used within the definition
-#   of Literals and Identifiers
-tokens = {'Whitespace': re.compile(r'\s+'),
-          'Semicolon': re.compile(r';'),
-          'Equals': re.compile(r'='),
-          'Left Paren': re.compile(r'\('),
-          'Right Paren': re.compile(r'\)'),
-          'Times': re.compile(r'\*'),
-          'Plus': re.compile(r'\+'),
-          'Minus': re.compile(r'-'),
-          'Identifier': re.compile(r'[a-zA-Z_]([a-zA-Z_]|[0-9])*'),
-          'Literal': re.compile(r'0|[1-9][0-9]*'),
-          'Invalid Input': re.compile('.')}
-
-
 class Tokenizer:
 
+    # dict contains the name of the token type (key) and a compiled regex (value) for all terminals in the toy language
+    tokens = {';': re.compile(r';'),
+              '=': re.compile(r'='),
+              '(': re.compile(r'\('),
+              ')': re.compile(r'\)'),
+              '*': re.compile(r'\*'),
+              '+': re.compile(r'\+'),
+              '-': re.compile(r'-'),
+              'Identifier': re.compile(r'[a-zA-Z_]([a-zA-Z_]|[0-9])*'),
+              'Literal': re.compile(r'0|[1-9][0-9]*'),
+              'Invalid': re.compile('.')}
+
+    # regular expression to match whitespace, which should be removed from input text
+    whitespace = re.compile(r'\s+')
+
+    # initialize tokenizer by removing whitespace from text and creating variables to track the current position
+    #   in the text, as well as the length of the text
     def __init__(self, text):
-        self.text = text
-        self.endPosition = len(text)
+        self.text = re.sub(self.whitespace, '', text)
+        self.endPosition = len(self.text)
         self.currentPosition = 0
 
-    def tokenize(self):
-        # continue to tokenize the string until the current position is at the end of the string
-        while self.currentPosition != self.endPosition:
-            for pattern in tokens:
-                # for all regex patterns in tokens dictionary, check for a match at current position
-                match = tokens[pattern].match(self.text, self.currentPosition)
-                if match:
-                    if pattern == 'Whitespace':
-                        # skip over whitespace
-                        self.currentPosition = match.end()
-                        break
-                    elif pattern == 'Invalid Input':
-                        # catches anything that does not match another pattern in tokens
-                        raise Exception('ERROR: unrecognized character ' + self.text[match.start():match.end()])
-                    else:
-                        # output matching string and corresponding token type
-                        print(self.text[match.start():match.end()] + ' --- ' + pattern)
-                        self.currentPosition = match.end()
-                        break
+    def read_next_token(self):
+        for pattern in self.tokens:
+            # for all regex patterns in tokens dictionary, check for a match at current position
+            match = self.tokens[pattern].match(self.text, self.currentPosition)
+            if match:
+                if pattern == 'Invalid':
+                    # catches anything that does not match another pattern in tokens
+                    raise Exception('ERROR: unrecognized character ' + self.text[match.start():match.end()])
+                else:
+                    # return tuple containing token (as a string) and the pattern type
+                    self.currentPosition = match.end()
+                    return {'token': self.text[match.start():match.end()], 'type': pattern}
 
 
 # an example of the tokenizer running on two different input strings
 try:
     t = Tokenizer('x = (-13 * 2) + var3;')
-    t.tokenize()
-    print('\n')
-    t = Tokenizer('0+B15 _B01 \n 9 \t 15/3')
-    t.tokenize()
+    while t.currentPosition != t.endPosition:
+        current_token = t.read_next_token()
+        print(current_token)
 except Exception as error:
     print(error)
     exit(1)
